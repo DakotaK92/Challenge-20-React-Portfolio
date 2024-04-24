a2fc7226660b457d62a6a98f60ce318b924a6c4d
DakotaK92 <46942706+DakotaK92@users.noreply.github.com>
2024-04-24 18:49:10 -0500

    Update intro on src/main

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Intro.css";
-import bg from '../../assets/icons/introPicFade.png';
+import bg from '../../assets/icons/introPicFade240.png';
 
 const Intro = () => {
     return(
         <section id="Intro">
             <div className="introContainer">
                 <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Dakota</span> <br />Full Stack Developer and Graphic Designer</span>
-                <p className="introPara">I create website and make them look good!</p>
+                <span className="introText">I'm <span className="introName">Dakota King</span>, <br />Full Stack Developer and Graphic Designer</span>
+                <p className="introPara">I create websites and make them look good!</p>
             </div>
             <img src={bg} alt="Profile" className="bg"/>
         </section>
